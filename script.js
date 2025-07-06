ebe018ef748a4b528188c0d9e87de2983b7567a0
Issa-Abbani <issaabbani04@gmail.com>
2025-07-06 17:00:41 +0300

    Added player choice

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-
-
+function getHumanChoice(){
+  let playerChoice;
+  do{
+   playerChoice = window.prompt("rock, paper, or scissors");
+  }while(playerChoice !== "rock" &&  playerChoice !== "paper" && playerChoice !== "scissors");
+  return playerChoice;
+}
 
